8acb62f271abf8019e6cf31a60941a48a0be693c
gamez20 <gamez.guerra18@gmail.com>
2022-08-23 11:53:22 -0500

    test

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         AirlineManager airlineTicketManager = new AirlineManager();
         airlineTicketManager.initManager();
+        
     }
 }
