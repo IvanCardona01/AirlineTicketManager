26d9885ebe951a11092474bbc19a06573d1c3278
gamez20 <gamez.guerra18@gmail.com>
2022-08-23 15:32:42 -0500

    javadoc

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
 import ManagerModule.controller.AirlineManager;
 
+/**
+ * Sistema de Gestion De Tickets
+ * @author Ivan Alexander Cardona Rodriguez
+ * @author Santiago Gamez Guerra
+ * @version  23/08/20222 1.0
+ */
+
 public class Main {
+    /**
+     * Inicio del sistema
+     * @param args
+     */
     public static void main(String[] args) {
         AirlineManager airlineTicketManager = new AirlineManager();
         airlineTicketManager.initManager();
-        
+
     }
 }
--- src/ManagerModule/controller/AirlineManager.java
+++ src/ManagerModule/controller/AirlineManager.java
@@ -57,11 +57,6 @@ public class AirlineManager {
     }
 
     public void addTicket(TicketForm form) {
-
-
-
-
-
         airlineTickets.add(form);
         int stand = form.getAirplaneStand().getStandNumber();
         airplane.ocupeStand(0, 0, 0, form.getAirplaneStand(), stand);
